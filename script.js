a85a3a1c4a3388e0cd8e0e8f3764cab03ea6f155
EAusman <emma.ausman@gmail.com>
2021-10-13 09:34:05 -0600

    joke features

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ fetch(url)
   return response.json();
 }).then(function(json){
   console.log(json);
-  
+  let joke;
+  let reply;
+  if(json.type === "twopart"){
+    joke=json.setup
+    reply=json.delivery
+    document.getElementById("joke").innerHTML = joke;
+    document.getElementById("answer").innerHTML = reply;
+  }
+  else{
+    joke=json.joke
+    document.getElementById("joke").innerHTML = joke;
+  }
 });
